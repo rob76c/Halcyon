2bfc20624c6e6a725ab21a0c6b21331231a4b948
Robbie <rob76c@gmail.com>
2024-10-12 22:57:12 -0400

    Capitalize

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function ExerciseListItem({ item }) {
@@ -5,8 +6,9 @@ export default function ExerciseListItem({ item }) {
       <View style={styles.exerciseContainer}>
         <Text style={styles.exerciseName}>{item.name}</Text>
         <Text style={styles.exerciseMuscleandEquipment}>
-          {item.muscle.toUpperCase()} | {item.equipment.toLocaleUpperCase()}
-        </Text>
+          <Text style={styles.subValue}>{item.muscle}</Text> | {' '}
+          <Text style={styles.subValue}>{item.equipment}</Text>
+        </Text> 
       </View>
     );
   }
@@ -24,6 +26,9 @@ export default function ExerciseListItem({ item }) {
     },
     exerciseMuscleandEquipment: {
       color: "dimgray",
+    },
+    subValue: {
+      textTransform: 'capitalize'
     },
   });
   
