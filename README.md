4ec781155550892675fec6fd84f23eae32aa1962
Robbie <rob76c@gmail.com>
2024-10-14 19:04:21 -0400

    starting stepzen/apininjas

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Fitness
+
+stepzen import curl https://api.api-ninjas.com/v1/exercises \
+-H "X-Api-Key: 8HJBtIifi9YGIBCjcrO2Zw==4eY5U0wkQtH99hCW"
--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,4 +1,4 @@
-import { Stack } from "expo-router"
+import { Stack, Slot } from "expo-router"
 import React from "react";
 
 export default function RootLayout () {
